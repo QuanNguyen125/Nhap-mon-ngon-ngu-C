b0c2d39b5af7890236263181d59237eda4815dbd
QuanNguyen125 <72533416+QuanNguyen125@users.noreply.github.com>
2020-12-17 08:08:26 +0700

    Update hoadon.c

--- Bài tập 3/hoadon.c
+++ Bài tập 3/hoadon.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main(void) {
+  // khai bao gia tri//
+  int a, b;
+  // khai bao don gia//
+  printf("Nhap don gia hang hoa: ");
+  scanf("%d",&a);
+  // khai bao so luong//
+  printf("Nhap so luong hang hoa: ");
+  scanf("%d",&b);
+  int dongia, giatri, thanhtien;
+  // cong thuc don gia * so luong = thanh tien//
+  thanhtien = a * b;
+  printf("thanh tien "),
+  printf("=%d",thanhtien);
+
+  return 0;
+}
