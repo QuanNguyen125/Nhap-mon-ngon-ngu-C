e1252f7165e6c781b90c1a35875183c5a238e279
QuanNguyen125 <72533416+QuanNguyen125@users.noreply.github.com>
2021-01-09 10:44:48 +0700

    Update kt_06.c

--- Bài tập 3/kt_06.c
+++ Bài tập 3/kt_06.c
@@ -4,7 +4,7 @@ int main(void) {
   int h;
   //h = 4
   //hàng1:1 ngôi sao;3 khoảng trống//
-  //hàng2:3 ngôi sao;3 khoảng trống//
+  //hàng2:3 ngôi sao;2 khoảng trống//
   //hàng3:5 ngôi sao;1 khoảng trống//
   //hàng4:7 ngôi sao;0 khoảng trống//
   //h = 3
