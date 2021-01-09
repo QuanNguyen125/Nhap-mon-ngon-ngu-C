5e295c55bfe135653bd9260e0e582b7af0f407e3
QuanNguyen125 <72533416+QuanNguyen125@users.noreply.github.com>
2021-01-09 10:42:36 +0700

    Create kt_06.c

---
+++ Bài tập 3/kt_06.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+  int h;
+  //h = 4
+  //hàng1:1 ngôi sao;3 khoảng trống//
+  //hàng2:3 ngôi sao;3 khoảng trống//
+  //hàng3:5 ngôi sao;1 khoảng trống//
+  //hàng4:7 ngôi sao;0 khoảng trống//
+  //h = 3
+  //hàng1:3 ngôi sao;2 khoảng trống//
+  //hàng2:5 ngôi sao;1 khoảng trống//
+  //hàng3:7 ngôi sao;0 khoảng trống//
+  //h = 2
+  //hàng1:5 ngôi sao;1 khoảng trống//
+  //hàng2:7 ngôi sao;0 khoảng trống//
+  //h = 1 
+  //hàng1:7 ngôi sao;0 khoảng trống//
+  return 0;
+  
+}
