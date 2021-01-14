c5ce3d109d8ba43260fa4c8596125193d6bfe7fd
QuanNguyen125 <72533416+QuanNguyen125@users.noreply.github.com>
2021-01-14 09:09:28 +0700

    Create mang_01.c

---
+++ Bài tập 4/mang_01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+  int i=0;
+  //khai bao mang
+  int arry[4]={3,5,7,2};
+  for(int i = 0; i < 4 ; i++)
+  printf("%d\n",arry[i]);
+
+  int tong;
+  // cong thuc tinh tong
+  tong = 3+5+7+2;
+  printf("tong=%d",tong);
+  return 0;
+}
