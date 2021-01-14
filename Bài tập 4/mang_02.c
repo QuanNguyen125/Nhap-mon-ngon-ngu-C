8fea3a9d00af8bb3246f2a5e9b58cba643387f05
QuanNguyen125 <72533416+QuanNguyen125@users.noreply.github.com>
2021-01-14 10:57:28 +0700

    Create mang_02.c

---
+++ Bài tập 4/mang_02.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+  //khai bao mang a kich thuoc n
+  int a[100];
+  int n;
+  printf("Nhap kich thuoc n cho mang a: ");
+  printf("nhap tung phan tu a kich thuoc n: ");
+  //khai bao mang
+  int arry[100];
+  for(int b = 0; b < 100 ; b++)
+  printf("%d\n",arry[b]);
+
+  int tong;
+  // cong thuc tinh tong
+  tong = 100;
+  printf("tong=%d",tong);
+  return 0;
+}
